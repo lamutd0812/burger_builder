5fc43568755837a6484323008db0abe90cf7b0f5
lamutd0812 <lamutd0812@outlook.com>
2020-03-17 22:19:37 +0700

    p7-119

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render(){
     return (
       <div>
-        <p>Hello React!</p>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,29 @@
+import React , { Component } from 'react';
+import Aux from '../../hoc/Auxiliary/Auxiliary';
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends Component{
+    // constructor(props){
+    //     super(props);
+    //     this.state = {...}
+    // }
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
+
+    render(){
+        return (
+            <Aux>
+                <Burger ingredients={this.state.ingredients} />
+                <div>Burger Control</div>
+            </Aux>
+        );  
+    }
+}
+
+export default BurgerBuilder;
